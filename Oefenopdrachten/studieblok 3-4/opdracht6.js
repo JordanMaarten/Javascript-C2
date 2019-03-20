8ce9dc32217ec2f42df2f1a159610d1e9249b084
JordanMaarten <124160@student.drenthecollege.nl>
2019-03-20 11:45:18 +0100

    tweaked "opdracht6.js"

--- Oefenopdrachten/studieblok 3-4/opdracht6.js
+++ Oefenopdrachten/studieblok 3-4/opdracht6.js
@@ -1,15 +1,15 @@
 function startTimer() {
-  var tijd = document.getElementsByName("tijd_input")[00].value;
+  var tijd = document.getElementsByName("tijd_input")[0].value;
   tijd *= 1000;
   if (tijd < 0 || tijd == 0 || tijd == "") {
-    window.alert("Number can only be 1 second or higher.");
-    document.getElementsByName("tijd_input")[00].value = "";
+    alert("Number can only be 1 second or higher.");
+    document.getElementsByName("tijd_input")[0].value = "";
   } else {
     setTimeout(stopTimer, tijd);
-    document.getElementsByName("tijd_input")[00].value = "";
+    document.getElementsByName("tijd_input")[0].value = "";
   }
 }
 
 function stopTimer() {
-  window.alert("Timer afgelopen!");
+  alert("Timer afgelopen!");
 }
