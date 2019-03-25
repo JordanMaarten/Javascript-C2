267e53dcbb06b82bed9cff88a798eaefb3647089
JordanMaarten <124160@student.drenthecollege.nl>
2019-03-25 18:04:09 +0100

    made "opdracht7.js" more pretty

--- Oefenopdrachten/studieblok 3-4/opdracht7.js
+++ Oefenopdrachten/studieblok 3-4/opdracht7.js
@@ -1,10 +1,15 @@
+var info = document.getElementById("info");
+var linebreak1 = document.getElementById("linebreak1");
+var button = document.getElementById("button");
+
 function verbergen() {
-  var info = document.getElementById("info");
-  if (info.style.display == "none") {
-    info.style.display = "block";
-    document.getElementById("button").innerHTML = "hide";
-  } else {
+  if (info.style.display == "block") {
     info.style.display = "none";
+    linebreak1.style.display = "block";
     document.getElementById("button").innerHTML = "show";
+  } else {
+    info.style.display = "block";
+    linebreak1.style.display = "none";
+    document.getElementById("button").innerHTML = "hide";
   }
 }
